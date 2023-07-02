c2c40e3d174185322de6525188381b7e460ca4ce
DESKTOP-FIFPP85 <amruthadondpati3@gmail.com>
2023-07-02 20:11:45 +0530

    catlogue updated

--- AP/catalogue.sh
+++ AP/catalogue.sh
@@ -6,8 +6,8 @@ source AP/common.sh
 echo -e "\e[32m -------------$COMPONENT has started---------------\e[0m"
 
 echo -e "\e[32m -----------nojdeJS repo started-----------\e[0m"
-curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash 
-yum install nodejs -y  
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash "{$LOFGILE}"
+yum install nodejs -y  "{$LOFGILE}"
 stat $?
 
 echo -e "add the user $APPUSER component"
