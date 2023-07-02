7f23558080ed4c838c87bbc0235a4319a9c932e5
DESKTOP-FIFPP85 <amruthadondpati3@gmail.com>
2023-07-02 17:27:30 +0530

    start updated

--- AP/frontend.sh
+++ AP/frontend.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
 
-echo -n "Installing Nginx"
-yum install nginx -y   &>> /tmp/frontend.log 
+COMPONENT=frontend
 
-if [ $? -eq 0 ]; then 
+stat() {
+    if [ $1 -eq 0 ]; then 
     echo -e "\e[32m Sucess \e[0m"
 else
     echo "\e[32m failed \e[0m"
 fi
+}
+
+echo -n "Installing Nginx"
+yum install nginx -y   &>> /tmp/frontend.log 
+stat $?
 
 echo -n "enabling Nginx"
 systemctl enable nginx &>> /tmp/frontend.log
-if [ $? -eq 0 ]; then 
-    echo -e "\e[32m Sucess \e[0m"
-else
-    echo "\e[32m failed \e[0m"
-fi
+stat $?
+
 echo -n "starting Nginx"
 systemctl start nginx  &>> /tmp/frontend.log
+stat $?
 
-if [ $? -eq 0 ]; then 
-    echo -e "\e[32m Sucess \e[0m"
-else
-    echo "\e[32m failed \e[0m"
-fi
+echo -n "Downloading the $COMPONENT"
+curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
+stat $?
+
+# cd /usr/share/nginx/html
+# rm -rf *
+# unzip /tmp/frontend.zip
+# mv frontend-main/* .
+# mv static/* .
+# rm -rf frontend-main README.md
+# mv localhost.conf /etc/nginx/default.d/roboshop.conf
