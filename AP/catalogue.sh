b77a9b4a3d41e281709cf3f633f09ff67084295c
DESKTOP-FIFPP85 <amruthadondpati3@gmail.com>
2023-07-02 20:01:53 +0530

    catlogue updated

--- AP/catalogue.sh
+++ AP/catalogue.sh
@@ -8,7 +8,7 @@ source AP/common.sh
 echo -e "\e[32m -------------$COMPONENT has started---------------\e[0m"
 
 echo -e "\e[32m -----------nojdeJS repo started-----------\e[0m"
-curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash - &>> $LOFGILE
 yum install nodejs -y  &>> $LOFGILE
 stat $?
 
