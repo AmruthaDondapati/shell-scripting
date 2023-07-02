2efe702d28678dc996b3228b0632e36296e88a6f
DESKTOP-FIFPP85 <amruthadondpati3@gmail.com>
2023-07-02 19:45:29 +0530

    catlogue updated

---
+++ AP/catalogue.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e 
+
+COMPONENT=catalogue
+source AP/common.sh
+
+echo -e "\e[32m -------------$COMPONENT has started---------------\e[0m"
+
+echo -e "\e[32m -----------nojdeJS repo started-----------\e[0m"
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
+yum install nodejs -y  &>> $LOFGILE
+stat $?
+
+echo -e "add the user $APPUSER component"
+useradd roboshop
+stat $?
