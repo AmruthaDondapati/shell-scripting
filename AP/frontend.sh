1896ba286e3caca9e5ab3b73d6affb07208096aa
DESKTOP-FIFPP85 <amruthadondpati3@gmail.com>
2023-07-02 11:33:31 +0530

    updated

--- AP/frontend.sh
+++ AP/frontend.sh
@@ -3,12 +3,23 @@
 COMPONENT=frontend
 source common.sh
 
+stat() {
+    if [ $1 -eq 0 ]; then 
+        echo -e "\e[32m Success \e[0m "
+    else 
+        echo -e "\e[31m failure \e[0m"
+    fi 
+}
+
 yum install nginx -y   &>> "${LOFGILE}"  
 systemctl enable nginx &>> "${LOFGILE}"
 systemctl start nginx
 
+stat $? 
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"  &>> "${LOFGILE}"
+
+stat $?
 
 # cd /usr/share/nginx/html
 # rm -rf *  
@@ -17,3 +28,4 @@ curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend
 # mv static/* .
 # rm -rf frontend-main README.md   &>> "${LOFGILE}" 
 # mv localhost.conf /etc/nginx/default.d/roboshop.conf 
+
--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
 frontend:
 	bash AP/frontend.sh
+
+	
