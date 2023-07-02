b27add8f9f57595215f7d42148b0419400d0aa0b
DESKTOP-FIFPP85 <amruthadondpati3@gmail.com>
2023-07-02 11:38:50 +0530

    updated

--- AP/frontend.sh
+++ AP/frontend.sh
@@ -22,11 +22,16 @@ curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend
 
 stat $?
 
-# cd /usr/share/nginx/html
-# rm -rf *  
-# unzip /tmp/frontend.zip  &>> "${LOFGILE}" 
-# mv frontend-main/* .
-# mv static/* .
-# rm -rf frontend-main README.md   &>> "${LOFGILE}" 
-# mv localhost.conf /etc/nginx/default.d/roboshop.conf 
+cd /usr/share/nginx/html
+rm -rf *  
+stat $?
+
+unzip /tmp/frontend.zip  &>> "${LOFGILE}" 
+stat $?
+
+mv frontend-main/* .
+mv static/* .
+rm -rf frontend-main README.md   &>> "${LOFGILE}" 
+mv localhost.conf /etc/nginx/default.d/roboshop.conf 
+stat $?
 
