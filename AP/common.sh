e1979db5e5322a377da30a2d3008b235e0b40119
DESKTOP-FIFPP85 <amruthadondpati3@gmail.com>
2023-07-02 18:31:16 +0530

    mongo db updated

--- AP/common.sh
+++ AP/common.sh
@@ -1,2 +1,15 @@
-APPUSER=roboshop
-LOFGILE="/tmp/${COMPONENT}.log"
+LOFGILE="/tmp/$COMPONENT.log"
+
+ID=$(id -u)
+if [ $ID -ne 0 ] ; then 
+    echo -e "\e[31m You need to script either as a root user or with a sudo privilege \e[0m"
+    exit 1
+fi 
+
+stat() {
+    if [ $1 -eq 0 ]; then 
+        echo -e "\e[32m Sucess \e[0m"
+    else
+        echo -e "\e[31m failed \e[0m"
+    fi
+}
---
+++ AP/mongodb.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+COMPONENT=mongodb
+
+echo -e "\e[32m -------------$COMPONENT has started---------------\e[0m"
+
+echo -n "Install repo"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
+stat $?
+
+echo -n "Install mongodb"
+yum install -y mongodb-org
+stat $?
+
+echo -n "start mongodb"
+systemctl enable mongod
+systemctl start mongod
+stat $?
+
+echo -n "loading zip file for schema"
+curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
+stat $?
+
+echop -n "extracting the zip file content"
+cd /tmp
+unzip mongodb.zip
+stat $?
+
+echo "moving the content to latest mongodb file"
+cd mongodb-main
+mongo < catalogue.js
+mongo < users.js
+stat $?
+
+echo -e "\e[32m -------------$COMPONENT compiled and completed successfully---------------\e[0m"
