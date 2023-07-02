168c35939e796a3651f6da161af4e556d05e97d5
DESKTOP-FIFPP85 <amruthadondpati3@gmail.com>
2023-07-02 19:37:50 +0530

    code updated

--- AP/frontend.sh
+++ AP/frontend.sh
@@ -2,15 +2,8 @@
 set -e
 
 COMPONENT=frontend
-LOFGILE=/tmp/$COMPONENT.log 
+source AP/common.sh 
 
-stat() {
-    if [ $1 -eq 0 ]; then 
-        echo -e "\e[32m Sucess \e[0m"
-    else
-        echo -e "\e[31m failed \e[0m"
-    fi
-}
 
 echo -e "\e[32m -------------$COMPONENT has started---------------\e[0m"
 
