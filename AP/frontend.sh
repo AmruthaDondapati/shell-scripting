bb881df34413585ee2bbc0b24bfefe747e1765f3
DESKTOP-FIFPP85 <amruthadondpati3@gmail.com>
2023-07-02 17:15:58 +0530

    start updated

--- AP/frontend.sh
+++ AP/frontend.sh
@@ -4,9 +4,9 @@ echo "Installing Nginx"
 yum install nginx -y   &>> /tmp/frontend.log 
 
 if [ $? -eq 0 ]; then 
-    echo "Sucess"
+    echo -e "\e[32m Sucess \e[0m]"
 else
-    echo "failed"
+    echo "\e[32m failed \e[0m]"
 fi
 
 echo "enabling Nginx"
@@ -14,3 +14,9 @@ systemctl enable nginx &>> /tmp/frontend.log
 
 echo "starting Nginx"
 systemctl start nginx  &>> /tmp/frontend.log
+
+if [ $? -eq 0 ]; then 
+    echo -e "\e[32m Sucess \e[0m]"
+else
+    echo "\e[32m failed \e[0m]"
+fi
