7c1de72c5cba310e4bd33389aab32c28f89cec12
DESKTOP-FIFPP85 <amruthadondpati3@gmail.com>
2023-07-02 17:08:32 +0530

    start updated

--- AP/frontend.sh
+++ AP/frontend.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "Installing Nginx"
 yum install nginx -y   &>> /tmp/frontend.log 
+
+echo "enabling Nginx"
 systemctl enable nginx &>> /tmp/frontend.log
+
+echo "starting Nginx"
 systemctl start nginx  &>> /tmp/frontend.log
