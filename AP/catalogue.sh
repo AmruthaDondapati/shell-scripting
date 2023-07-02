4f6bd8704c3b7984f75e956e45ddcfec0ef615fd
DESKTOP-FIFPP85 <amruthadondpati3@gmail.com>
2023-07-02 21:15:13 +0530

    catlogue updated

--- AP/catalogue.sh
+++ AP/catalogue.sh
@@ -3,9 +3,9 @@
 COMPONENT=catalogue
 source AP/common.sh
 
-echo -n "\e[32m -------------$COMPONENT has started---------------\e[0m"
+echo -n -e "\e[32m -------------$COMPONENT has started---------------\e[0m"
 
-echo -n "\e[32m -----------nojdeJS repo started-----------\e[0m"
+echo -n -e "\e[32m -----------nojdeJS repo started-----------\e[0m"
 curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash &>> $LOFGILE
 yum install nodejs -y  &>> $LOFGILE
 stat $?
@@ -18,12 +18,12 @@ if [ $? -ne 0 ] ; then
 fi
 
 echo -n "download the $COMPONENT"
-$ curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/catalogue/archive/main.zip"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/catalogue/archive/main.zip"
 stat $?
 
 echo -n "Extracting the $COMPONENT"
-$ cd /home/$APPUSER
-$ unzip -o /tmp/$COMPONENT.zip  &>> $LOFGILE 
+cd /home/$APPUSER
+unzip -o /tmp/$COMPONENT.zip  &>> $LOFGILE 
 stat $?
 
 
