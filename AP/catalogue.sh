f953a2f60c19c7fba87087ef0956d548bbfc1571
DESKTOP-FIFPP85 <amruthadondpati3@gmail.com>
2023-07-17 14:28:11 +0530

    head line updated

--- AP/catalogue.sh
+++ AP/catalogue.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 COMPONENT=catalogue
-source AP/common.sh
+
+LOFGILE="/tmp/$COMPONENT.log"
+APPUSER=roboshop
+
+ID=$(id -u)
+if [ $ID -ne 0 ] ; then 
+    echo -e "\e[31m You need to script either as a root user or with a sudo privilege \e[0m"
+    exit 1
+fi 
+
+stat() {
+    if [ $1 -eq 0 ]; then 
+        echo -e "\e[32m Sucess \e[0m"
+    else
+        echo -e "\e[31m failed \e[0m"
+    fi
+}
+
 
 echo -n "Configuring NodeJS Repo :"
     curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -  &>> "${LOFGILE}"
