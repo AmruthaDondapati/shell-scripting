adf91a2e57889786727d6f7b0344d2f07647bdf0
Amrutha <amruthadondapati3@gmail.com>
2023-12-03 20:31:53 +0530

    did

--- launch-ec2.sh
+++ launch-ec2.sh
@@ -3,7 +3,7 @@
 
 COMPONENT=$1 
 ENV=$2
-HOSTED_ZONE_ID="Z090521761DHPU3HXLNP"
+HOSTED_ZONE_ID="Z07696082GU4245NS3UII"
 
 if [ -z "$COMPONENT" ] || [ -z "$ENV" ]; then 
     echo -e "\e[31m Component name is required \n Sample Usage: \n\n\t\t bash launch-ec2.sh componentName envName  \e[0m"
@@ -12,15 +12,15 @@ fi
 
 # AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=DevOps-LabImage-CentOS7" --region us-east-1 | jq .Images[].ImageId | sed -e 's/"//g')
 AMI_ID="ami-0f75a13ad2e340a58"
-aws ec2 create-default-subnet --availability-zone us-east-1a
-SG_ID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=b52-allow-all --region us-east-1 | jq .SecurityGroups[].GroupId | sed -e 's/"//g')
+SG_ID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=My_Batch_52 --region us-east-1 | jq .SecurityGroups[].GroupId | sed -e 's/"//g')
 
 echo -e "AMI ID Used to launch the instance is \e[32m $AMI_ID \e[0m "
 echo -e "Security Group ID Used to launch the instance is \e[32m  $SG_ID \e[0m"
 
 launch_ec2() { 
 
-    echo "______ $COMPONENT launch is in progress ______"
+    echo  -e "\e[33m______ $COMPONENT launch is in progress ______\e[0m"
+    aws ec2 create-default-subnet --availability-zone us-east-1a &>> "${LOFGILE}"
     PRIVATE_IP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro  --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}-${ENV}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
     echo -e "Private_ip of the $COMPONENT-${ENV} Server is \e[32m $PRIVATE_IP \e[0m"
@@ -30,7 +30,7 @@ launch_ec2() {
     sed -e "s/IPADDRESS/$PRIVATE_IP/" -e "s/COMPONENT/$COMPONENT-${ENV}/" route53.json  > /tmp/r53.json 
     aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --change-batch file:///tmp/r53.json 
 
-    echo -n "______ Internal DNS Record for $COMPONENT-${ENV} is completed __________"  
+    echo -e "\e[33m______ Internal DNS Record for $COMPONENT-${ENV} is completed __________\e[0m"
 
 }
 
@@ -41,5 +41,5 @@ if [ "$1" == "all" ]; then
         launch_ec2                  # Calling Create Server Function
     done 
 else 
-        launch_ec2                  # Calling Create Server Function
-fi 
+        launch_ec2                  # Calling Create Server Function
+fi
