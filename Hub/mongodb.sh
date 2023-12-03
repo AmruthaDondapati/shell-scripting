73c6990844efc86d5a8a840e213a000dcc0dc762
Amrutha <amruthadondapati3@gmail.com>
2023-12-03 19:07:40 +0530

    did

--- Hub/mongodb.sh
+++ Hub/mongodb.sh
@@ -2,7 +2,7 @@
 
 COMPONENT=mongodb
 
-source components/common.sh    # Source loads a file and this file has all the common patterns.
+source Hub/common.sh    # Source loads a file and this file has all the common patterns.
 
 echo -n "Downloading $COMPONENT : "
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
