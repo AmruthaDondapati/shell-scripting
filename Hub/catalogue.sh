7c3118e7e74fcba469f9e0a7aaa5d42db21a7147
Amrutha <amruthadondapati3@gmail.com>
2023-12-03 19:12:16 +0530

    did

--- Hub/catalogue.sh
+++ Hub/catalogue.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 
 COMPONENT=catalogue
-source components/common.sh    # Source loads a file and this file has all the common patterns.
+source Hub/common.sh    # Source loads a file and this file has all the common patterns.
 NODEJS                         # Calling NodeJS Function
